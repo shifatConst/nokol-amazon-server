57cf726409df647575d83fdfddace792d35ce4f3
shifatreza <shifatreza44@gmail.com>
2021-03-28 12:38:06 +0530

    root directory added

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 5000;
 
+app.get('/', (req, res) => {
+    res.send('back-end root directory');
+})
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
